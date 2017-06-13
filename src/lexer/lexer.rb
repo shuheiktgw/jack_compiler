d73ba59634b3ab0814b30e1a1067f3316030727a
Shuhei Kitagawa <shuhei.kitagawa@c-fo.com>
2017-06-13 10:23:47 +0900

    defineing lexer

--- src/lexer/lexer.rb
+++ src/lexer/lexer.rb
@@ -1,8 +1,56 @@
+require_relative '../token/token'
+
 class Lexer
-  def initialize(file)
-    @file = file
+
+  EOF = 'EOF'
+
+  def initialize(input)
+    @input = input # expect to receive return value of File.read('file_path')
     @position = 0
     @read_position = 0
     @char = ''
+
+    read_char
+  end
+
+  def next_token
+    eat_white_spaces
+
+    token = case @char
+    when '='
+      Token.new(token_type: Token.TOKEN_TYPE[:ASSIGN], literal: @char)
+    when '+'
+      Token.new(token_type: Token.TOKEN_TYPE[:PLUS], literal: @char)
+    when '-'
+      Token.new(token_type: Token.TOKEN_TYPE[:MINUS], literal: @char)
+    when '*'
+      Token.new(token_type: Token.TOKEN_TYPE[:ASTERISK], literal: @char)
+    when '/'
+      Token.new(token_type: Token.TOKEN_TYPE[:SLASH], literal: @char)
+    end
+
+
+  end
+
+
+  private
+
+  def read_char
+    if @read_position >= @input.length
+      @char = EOF
+    else
+      @char = @input[@read_position]
+    end
+
+    @position = @read_position
+    @read_position += 1
+  end
+
+  def eat_white_spaces
+    white_spaces = [' ', "\t", "\n", "\r"]
+
+    while white_spaces.include? @char
+      read_char
+    end
   end
 end
--- src/token/token.rb
+++ src/token/token.rb
@@ -1,6 +1,39 @@
 class Token
   TOKEN_TYPE = {
-
+    ILLEGAL: 'ILLEGAL',
+    EOF: 'EOF',
+    IDENT: 'IDENT',
+    INT: 'INT',
+    ASSIGN: '=',
+    PLUS: '+',
+    MINUS: '-',
+    BANG: '!',
+    ASTERISK: '*',
+    SLASH: '/',
+    LT: '<',
+    GT: '>',
+    EQ: '::',
+    NOT_EQ: '!:',
+    COMMA: ',',
+    SEMICOLON: ';',
+    LPAREN: '(',
+    RPAREN: ')',
+    LBRACE: '{',
+    RBRACE: '}',
+    FUNCTION: 'FUNCTION',
+    LET: 'LET',
+    TRUE: 'TRUE',
+    FALSE: 'FALSE',
+    IF: 'IF',
+    ELSE: 'ELSE',
+    RETURN: 'RETURN'
   }
+
+  attr_reader :token_type, :literal
+
+  def initialize(token_type:, literal:)
+    @token_type = token_type
+    @literal = literal
+  end
 
 end
