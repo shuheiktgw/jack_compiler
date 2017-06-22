3bbfe68c5f5ef886c6513408a058d015b378d841
Shuhei Kitagawa <shuhei.kitagawa@c-fo.com>
2017-06-22 10:49:05 +0900

    decided to start from parser not from ast...

--- src/parser/parser.rb
+++ src/parser/parser.rb
@@ -1,3 +1,5 @@
+require_relative '../token/token'
+
 class Parser
   def initialize(lexer)
     @lexer = lexer
@@ -8,9 +10,28 @@ class Parser
     next_token
   end
 
+  def parse_program
+    statements = []
+
+    while @current_token != Token::EOF
+      stmt = parse_statement
+      statements << stmt if stmt
+
+      next_token
+    end
+
+    statements
+  end
+
+  private
+
   def next_token
     @current_token = @next_token
     @next_token = @lexer.next_token
+  end
+
+  def parse_statement
+    
   end
 
 end
