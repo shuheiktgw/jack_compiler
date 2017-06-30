9556837478c482ef5e5b54e106b96003866b3949
Shuhei Kitagawa <shuhei.kitagawa@c-fo.com>
2017-06-30 09:53:29 +0900

    add tests for boolean parser

--- spec/parser/parser_spec.rb
+++ spec/parser/parser_spec.rb
@@ -35,14 +35,27 @@ describe Parser do
         end
       end
 
-      context 'expression is integer constant' do
-        let(:expressions) { '432718;' }
+      context 'expression is boolean constant' do
+        context 'when true' do
+          let(:expressions) { 'true;' }
 
-        it 'should return IntegerLiteral' do
-          expression = IntegerLiteral.new(token: Token.new(type: Token::INT, literal: '432718'), value: 432718)
-          expected = LetStatement.new(token: token, identifier: identifier, expression: expression)
+          it 'should return BooleanLiteral with true' do
+            expression = BooleanLiteral.new(token: Token.new(type: Token::TRUE, literal: 'true'), value: true)
+            expected = LetStatement.new(token: token, identifier: identifier, expression: expression)
 
-          expect(first_result).to eq expected
+            expect(first_result).to eq expected
+          end
+        end
+
+        context 'when false' do
+          let(:expressions) { 'false;' }
+
+          it 'should return BooleanLiteral with true' do
+            expression = BooleanLiteral.new(token: Token.new(type: Token::FALSE, literal: 'false'), value: false)
+            expected = LetStatement.new(token: token, identifier: identifier, expression: expression)
+
+            expect(first_result).to eq expected
+          end
         end
       end
     end
