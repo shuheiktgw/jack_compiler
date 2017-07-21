c3a9894030d05eec384d7a6f5428bb80c15caa39
Shuhei Kitagawa <shuhei.kitagawa@c-fo.com>
2017-07-21 10:09:33 +0900

    add to_h for var declarations

--- src/ast/declaration/var_declaration.rb
+++ src/ast/declaration/var_declaration.rb
@@ -6,4 +6,24 @@ class VarDeclaration < AstBase
     @type = type
     @identifier = identifier
   end
+
+  def to_h
+    {
+      varDec: [
+        {keyword: 'var'},
+        parse_type(type),
+        {identifier: identifier.literal},
+        {symbol: ';'}
+      ]
+    }
+
+  end
+
+  def parse_type(token)
+    if token.type == Token::IDENT
+      return { identifier: token.literal }
+    end
+
+    { keyword: token.literal }
+  end
 end
