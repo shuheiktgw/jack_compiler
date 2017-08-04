892f4c263b90c291be924f5ce1a62a7edbac6d99
Shuhei Kitagawa <shuhei.kitagawa@c-fo.com>
2017-08-04 10:21:31 +0900

    implemented generator for while statement

--- src/ast/statement/while_statement.rb
+++ src/ast/statement/while_statement.rb
@@ -9,4 +9,19 @@ class WhileStatement < AstBase
     @condition = condition
     @consequence = consequence
   end
+
+  def to_vm(generator)
+    to_start = generator.generate_label
+    to_end   = generator.generate_label
+
+    generator.write_label(to_start)
+    condition.to_vm(generator)
+
+    generator.write_command('~')
+    generator.write_if_goto(to_end)
+
+    consequence.to_vm(generator)
+    generator.write_goto(to_start)
+    generator.write_label(to_end)
+  end
 end
