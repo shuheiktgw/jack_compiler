4d77eb1b4a7085c40e2c9b4534af61c5c0aef3b2
Shuhei Kitagawa <shuhei.kitagawa@c-fo.com>
2017-08-13 20:18:10 +0900

    fixed small bugs in if statemsnt

---
+++ spec/ast/statement/if_statement_spec.rb
@@ -0,0 +1,41 @@
+require 'spec_helper'
+require './spec/to_vm_helper'
+
+describe IfStatement do
+  include ToVmHelper
+
+  setup_generator
+  subject{ File.read(path) }
+
+  describe '#to_vm' do
+    before do
+      IfStatement.new(token: 'token', condition: condition, consequence: consequence, alternative: alternative).to_vm(generator)
+    end
+
+    context 'without return value' do
+      let(:true_term) { BooleanLiteral.new(token: Token.new(type: Token::TRUE, literal: 'true'), value: true) }
+      let(:condition) { true_term }
+      let(:consequence) { ReturnStatement.new(token: 'token', return_value: true_term) }
+      # let(:alternative) { ReturnStatement.new(token: 'token', return_value: true_term) }
+
+      context 'without alternative' do
+        let(:alternative) { nil }
+
+
+        it do
+          expected = '''push constant 1
+neg
+not
+if-goto TestClass1
+push constant 1
+neg
+return
+goto TestClass1
+label TestClass1
+'''
+          is_expected.to eq expected
+        end
+      end
+    end
+  end
+end
--- src/ast/statement/if_statement.rb
+++ src/ast/statement/if_statement.rb
@@ -19,15 +19,19 @@ class IfStatement < AstBase
     to_end = generator.generate_label
 
     generator.write_command('~')
-    alternative ? generator.write_if_goto(to_else) : generator.write_if_goto(to_end)
+    alternative ? generator.write_if(to_else) : generator.write_if(to_end)
 
     # consequence
     consequence.to_vm(generator)
     generator.write_goto(to_end)
 
     # alternative
-    generator.write_label(to_else)
-    alternative.to_vm(generator)
+    if alternative
+      generator.write_label(to_else)
+      alternative.to_vm(generator)
+      generator.write_label(to_end)
+    end
+
     generator.write_label(to_end)
   end
 end
